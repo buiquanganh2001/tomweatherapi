2784a0da71a97e684f2889ad9bb73e87d90aa41e
Quanh Bui <37506972+buiquanganh2001@users.noreply.github.com>
2021-07-17 11:31:48 +0700

    first

---
+++ fetch.js
@@ -0,0 +1,17 @@
+class Fetch {
+    async getCurrent(input) {
+      const myKey = "80a3464292a9992080865f586bee2bd8";
+  
+      //make request to url
+  
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=${myKey}`
+      );
+  
+      const data = await response.json();
+  
+      console.log(data);
+  
+      return data;
+    }
+  }
